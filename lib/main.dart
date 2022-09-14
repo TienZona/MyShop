ab5f7ade6fa17bb28cceeda2deeb3a45e8617fb1
TienZona <tienzona001@gmail.com>
2022-09-14 15:08:07 +0700

    Xay dung trang chi tiet san pham

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+
+import 'ui/products/products_manager.dart';
+import 'ui/products/product_detail_screen.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -20,54 +24,12 @@ class MyApp extends StatelessWidget {
           secondary: Colors.deepOrange
         ),
       ),
-      home: Container(
-        color: Colors.green
-      ),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-  return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      home: SafeArea(
+        child: ProductDetailScreen(  
+          ProductsManager().items[0]
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), 
+      )
     );
   }
 }
+
