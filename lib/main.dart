81e828e6cf21cdbde8771d454433913849ea76eb
TienZona <tienzona001@gmail.com>
2022-09-14 15:53:45 +0700

    Xay dung trang tong quan cac san pham

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'ui/products/products_manager.dart';
 import 'ui/products/product_detail_screen.dart';
-
+import 'ui/products/product_overview_screen.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -25,9 +25,7 @@ class MyApp extends StatelessWidget {
         ),
       ),
       home: SafeArea(
-        child: ProductDetailScreen(  
-          ProductsManager().items[0]
-        ),
+        child: ProductsOverviewScreen(),
       )
     );
   }
