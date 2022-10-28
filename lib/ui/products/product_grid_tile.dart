37c18436245fb21dba95ae5111875fede3ddc632
TienZona <tienzona001@gmail.com>
2022-10-28 21:38:52 +0700

    Luu san pham yeu thich len Realtime Database cua Firebase

--- lib/ui/products/product_grid_tile.dart
+++ lib/ui/products/product_grid_tile.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:myapp/ui/cart/cart_manager.dart';
+import 'package:myapp/ui/screens.dart';
 import 'package:provider/provider.dart';
 import '../../models/product.dart';
-
-import 'product_detail_screen.dart';
 
 class ProductGridTile extends StatelessWidget {
   const ProductGridTile(  
@@ -49,7 +47,7 @@ class ProductGridTile extends StatelessWidget {
               ),
               color: Theme.of(context).colorScheme.secondary,
               onPressed: () {
-                product.isFavorite = !isFavorite;
+                  ctx.read<ProductsManager>().toggleFavoriteStatus(product);
               },
           );
         },
